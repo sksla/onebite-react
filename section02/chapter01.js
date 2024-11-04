67adc680d001cd1d1ed1f4a98869e77c6c594b7b
sksla <153481660+sksla@users.noreply.github.com>
2024-11-05 01:31:25 +0900

    seciont02_chapter1~2
    1. Truthy와 Falsy
    : JavaScript의 모든 값은 Truthy 하거나 Falsy함
    => 이를 이용하면 조건문 간결하게 만들기 가능
    Falsy한 값 : undefined, null, 0, -0, NaN, "", 0n
    Truth한 값 : 7가지 Falsy한 값 제외한 나머지들
    
    2. 단락 평가(Short-circuit Evaluation)
    : 논리 연산자(&&, ||)를 사용할 때 표현식의 왼쪽만으로 연산 결과 확정할 수 있는 경우 오른쪽은 아예 접근 X
    =>Truthy 또는 Falsy한 값도 적용됨
    
    +) && (AND) 연산자는 모든 피연산자가 참(또는 truthy)일 경우에 마지막 피연산자를 반환함.

--- section02/chapter01.js
+++ section02/chapter01.js
@@ -1,13 +1,27 @@
-// 1. Falsy한 값
+// JavaScript에서는 참, 거짓이 아닌 값도 참, 거짓으로 평가한다
+/* 
+  * Truthy & Falsy란?
+  - 참이나 거짓을 의미하지 않는 값도, 조건문 내에서 참이나 거짓으로 평가하는 특징
+  - JavaScript의 모든 값은 Truthy 하거나 Falsy함 
+    ==> 이를 이용하면 조건문을 간결하게 만들 수 있음
+
+*/
+// 1. Falsy한 값 (거짓 같은 값)
 let f1 = undefined;
 let f2 = null;
 let f3 = 0;
 let f4 = -0;
 let f5 = NaN;
 let f6 = "";
-let f7 = 0n;
+let f7 = 0n; // big Integer : 아주아주 큰 숫자 저장할 때 (알아만 두자)
 
-// 2. Truthy 한 값
+/*
+if (!f7) {
+  console.log("falsy");
+}
+*/
+
+// 2. Truthy 한 값 (참 같은 값)
 // -> 7가지 Falsy 한 값들 제외한 나머지 모든 값
 let t1 = "hello";
 let t2 = 123;
@@ -15,7 +29,25 @@ let t3 = [];
 let t4 = {};
 let t5 = () => {};
 
+/*
+if (t1) {
+  console.log("Truthy");
+}
+*/
+
 // 3. 활용 사례
+// Truthy, Falsy 이용 X
+/*
+function printName(person) {
+  if (person === undefined || person === null) {
+    console.log("person의 값이 없음");
+    return;
+  }
+  console.log(person.name);
+}
+*/
+
+// Truthy, Falsy 이용 O
 function printName(person) {
   if (!person) {
     console.log("person의 값이 없음");
--- section02/chapter02.js
+++ section02/chapter02.js
@@ -1,9 +1,69 @@
+/*
+  * 단락 평가(Short-circuit Evaluation)이란?
+    - AND 연산자(&&) 또는 OR 연산자(||) 같은 논리 연산식에서 첫번째 피연산자의 값만으로도 연산의 결과를 확정할 수 있다면
+      두번째 피연산자의 값에는 아예 접근하지 않는 특징
+      논리 연산자(&&, ||)를 사용할 때 표현식의 왼쪽에서 오른쪽으로 평가하며, 필요한 경우에만 평가를 중단함
+      ==> 이 방법을 통해 불필요한 계산을 피하고 효율적으로 값을 얻을 수 있습니다.
+
+    let varA = false;
+    let varB = true;
+
+    > AND 연산자 => 왼쪽 값이 false일 때 오른쪽을 평가하지 않고 바로 false 반환
+    console.log(varA && varB);
+
+    > OR 연산자 => 왼쪽 값이 true일 때 오른쪽을 평가하지 않고 바로 true 반환
+    console.log(varB || varA);
+
+*/
+
+// 특정 조건에 맞춰서 함수를 아예 호출하지 않도록 방지할 수 있는 기능을 가지고 있음
+// Truthy 또는 Falsy한 값도 적용됨
+/*
+function returnFalse() {
+  console.log("False 함수");
+  //return false;
+  return undefined; // Falsy한 값
+}
+
+function returnTrue() {
+  console.log("True 함수");
+  //eturn true;
+  return 10; // Truthy한 값
+}
+
+console.log(returnFalse() && returnTrue()); // returnTrue함수는 호출 X (단락 평가)
+console.log(returnTrue() && returnFalse()); // 두 개의 함수 모두 호출(단락 평가 작동 X)
+console.log(returnTrue() || returnFalse()); // returnFalse함수는 호출 X (단락 평가)
+*/
+
 // 단락 평가 활용 사례
+
+/*
+// 단락 평가 활용 전
+function printName(person) {
+  if (!person) {
+    console.log("person에 값이 없음");
+  }
+  console.log(person.name);
+}
+*/
+
+// 단락 평가 활용 후
+
+/*
+function printName(person) {
+  console.log(person && person.name);
+}
+*/
 
 function printName(person) {
   const name = person && person.name;
   console.log(name || "person의 값이 없음");
 }
 
-printName();
-printName({ name: "이정환" });
+printName(); // "person의 값이 없음"
+printName({ name: "이정환" }); // "이정환"
+
+// Truthy1 || Truthy2 ==> 첫번째 Truthy한 값이 반환
+// Truthy1 && Truthy2 ==> 두번째 Truthy한 값이 반환
+// && (AND) 연산자는 **모든 피연산자가 참(또는 truthy)**일 경우에 마지막 피연산자를 반환함.
