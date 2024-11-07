fc6e9e22997776f237ed136933538b03a18ae6ec
sksla <153481660+sksla@users.noreply.github.com>
2024-11-07 22:23:50 +0900

    section08_프로젝트2.투두리스트
    - 프로젝트 준비
    - UI 구현
    - 기능 구현 준비
    > 임시 데이터 (기본 데이터) => 계속 렌더링할 필요 없으니 app 컴포넌트 밖에 선언

---
+++ section08/src/App.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import { useState, useEffect, useRef } from "react";
+
+// 한번만 생성되면 되서 외부에 선언
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState([mockData]);
+
+  return (
+    <div className="App">
+      <section>
+        <Header />
+      </section>
+      <section>
+        <Editor />
+      </section>
+      <section>
+        <List />
+      </section>
+    </div>
+  );
+}
+
+export default App;
