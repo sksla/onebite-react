a54ddfc7209a18a11b434a2a550bc46ebb27c93e
sksla <153481660+sksla@users.noreply.github.com>
2024-11-08 22:41:26 +0900

    section08_투두리스트(Create-투두 추가하기)
    투두 추가하기
    *App.jsx :
    - Todo의 index 역할을 할 useRef 선언(=> 중복글 구분 위해, 임시데이터 id와 겹치지 않게 설정)
    - Todo 입력값 가져와서 기존의 데이터 객체로 만드는 함수 생성
    
    * Editor.jsx
    - App.jsx에서 onCreate함수 받아옴(구조분해할당)
    - input 입력값을 저장할 state선언
    - <input> 입력값을 content state에 저장하기 위한 변경 함수 생성
    - Enter를 치면 Todo 등록하는 함수
    -

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -7,10 +7,10 @@ import { useState, useEffect, useRef } from "react";
 // 임시 데이터(기본 데이터) : 계속 렌더링할 필요 없으니 App 컴포넌트 밖에 선언, 데이터가 어떤 형태로 있어야 하는지 설정
 const mockData = [
   {
-    id: 0,
-    isDone: false,
-    content: "React 공부하기",
-    date: new Date().getTime(),
+    id: 0, // index
+    isDone: false, // 체크박스
+    content: "React 공부하기", // todo 내용
+    date: new Date().getTime(), // todo 추가 시간
   },
   {
     id: 1,
@@ -28,6 +28,24 @@ const mockData = [
 
 function App() {
   const [todos, setTodos] = useState([mockData]);
+  // Todo의 index 역할을 할 useRef 선언
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      // 이 데이터 객체를 todos state 배열해 추가해야 함
+      id: idRef.current++, // 새로 부여한 index를 증가시켜 id에 부여
+      isDone: false,
+      content: content, // props로 받은 todo 입력값을 content에 부여
+      date: new Date().getTime(),
+    };
+
+    // state값 업데이트 하기
+    // state의 값은 상태변화함수를 호출해서만 수정 가능 => 그래야 변경된 state의 값을 리액트가 감지할 수 있음
+    // => todos.push{newTodo} (X) : 직접 변경 불가
+    // spread 연산자(...)로 기존의 todos state 배열의 값을 펼쳐준 후 추가할 newTodo 데이터를 넣는다.
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <div className="App">
@@ -35,7 +53,8 @@ function App() {
         <Header />
       </section>
       <section>
-        <Editor />
+        {/* Editor.jsx에 props로 onCreate함수 전달 */}
+        <Editor onCreate={onCreate} />
       </section>
       <section>
         <List />
--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,50 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  // <input> 입력값을 저장할 state 선언
+  const [content, setContent] = useState("");
+
+  // 입력칸이 공백일 때 추가버튼을 누르면 입력칸에 포커싱을 주기 위한 ref 선언
+  const contentRef = useRef();
+
+  // <input> 입력값을 content state에 저장하기 위한 변경함수
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // Enter를 치면 Todo 등록하는 함수
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  // Enter 또는 버튼 클릭시 실행되는 함수
+  const onSubmit = () => {
+    // 빈 문자열 추가 방지 & 입력칸 포커싱
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+
+    // App.jsx의 onCreate 함수에 인자값으로 content 전달하여 실행 => 위에서 props로 onCreate 함수 받아와서 가능
+    onCreate(content);
+
+    // Todo 추가 후 input 입력칸 비우기(content State 초기화)
+    setContent("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef} // 입력칸 포커싱을 위한 ref 연결
+        value={content}
+        onKeyDown={onKeydown} // 사용자가 키를 눌렀을 때 동작
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
