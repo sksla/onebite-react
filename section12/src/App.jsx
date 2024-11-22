b2e96a947da06fe25bbb5e108d858fbc17abddaa
sksla <153481660+sksla@users.noreply.github.com>
2024-11-23 03:12:18 +0900

    section12_감정일기장:폰트,이미지,레이아웃 설정
    폰트 설정
    이미지 설정
    - 이미지 최적화
    레이아웃 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,24 +4,22 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+// 감정 이미지 임포트
+/*
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
+*/
+// 이미지 불러오는 코드들을 별도의 모듈로 분리함 => src/util/get-emotion-image.js
+import { getEmotionImage } from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 /*
-  !! 주의사항 !!
-  1. Routes 컴포넌트 안에는 Route컴포넌트만 들어갈 수 있음
-  2. Routes 컴포넌트 바깥에 있는 요소는 경로와는 상관없이 모든 페이지에서 렌더링 됨
-
-  * Link : a 태그와 같은 역할을 함, 페이지 깜빡임 없음
-      - a 태그 사용시 페이지 깜빡임 발생
-
-
-  => 리액트 내부에서 페이지를 이동해야할 때 Link 태그를 이용하자
-
-  * useNavigate
-
-  * 동적 경로 (Dynamic Segments)
+  
 */
 function App() {
   const nav = useNavigate();
@@ -32,6 +30,14 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
