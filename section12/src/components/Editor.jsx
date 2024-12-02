8b80d17b929c01284c71cc1e0e276ce678fc4e47
sksla <153481660+sksla@users.noreply.github.com>
2024-12-02 13:30:38 +0900

    section12_감정일기장: New 페이지 기능 구현
    - 하이라이팅 기능
    - 페이지 이동(뒤로가기)
    - 새로운 일기 아이템 추가

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,6 +1,8 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const emotionList = [
   {
@@ -25,34 +27,102 @@ const emotionList = [
   },
 ];
 
-const Editor = () => {
-  const emotionId = 1;
+// date객체를 문자열로 변환
+const getStringedDate = (targetDate) => {
+  // 날짜 -> YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
+
+// onSubmit 함수 ==> new 페이지 : onCreate(새로운 일기 생성)
+//                  edit 페이지 : onUpdate(기존 일기 수정)
+const Editor = ({ onSubmit }) => {
+  // state를 객체 형태로 => 여러 개의 값을 하나의 state에 저장하고 관리할 수 있음
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  const nav = useNavigate();
+
+  // 날짜 onChange 이벤트 핸들러
+  const onChangeInput = (e) => {
+    let name = e.target.name; // 어떤 요소에 입력이 들어온건지(요소의 name속성값)
+    let value = e.target.value; // 입력된 값이 무엇인지?(요소의 value값)
+
+    if (name === "createdDate") {
+      value = new Date(value); // 문자열값을 날짜객체로 변경
+    }
+
+    setInput({
+      ...input, // 기존의 state값 유지
+      [name]: value,
+    });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
         <div className="emotion_list_wrapper">
           {emotionList.map((item) => (
             <EmotionItem
+              onClick={() =>
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: item.emotionId,
+                  },
+                })
+              }
               key={item.emotionId}
               {...item}
-              isSelected={item.emotionId === emotionId}
+              isSelected={item.emotionId === input.emotionId}
             />
           ))}
         </div>
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea
+          name="content"
+          value={input.content}
+          onChange={onChangeInput}
+          placeholder="오늘은 어땠나요?"
+        />
       </section>
       <section className="button_section">
-        <Button text={"취소하기"} />
-        <Button text={"작성완료"} type={"POSITIVE"} />
+        <Button onClick={() => nav(-1)} text={"취소하기"} />
+        <Button
+          onClick={onClickSubmitButton}
+          text={"작성완료"}
+          type={"POSITIVE"}
+        />
       </section>
     </div>
   );
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,15 +1,27 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
+  const nav = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+    // replace 옵션 : 뒤로 가기를 방지하면서 페이지를 이동시키는 옵션
+  };
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
