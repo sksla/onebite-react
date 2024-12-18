6df479fbcb961cd33422b2eefbf722d78f875ebb
sksla <153481660+sksla@users.noreply.github.com>
2024-12-18 23:08:06 +0900

    section01_ch06:형변환
    묵시적 형변환
    암묵적 형변환

--- section01/chapter06.js
+++ section01/chapter06.js
@@ -1,6 +1,8 @@
-// 형 변환 (Type Casting) : 어떤 값의 타입을 다른 타입으로 변경함
-// 묵시적 형 변환 (암묵적 형 변환) : 알아서 자바스크립트 엔진이 형 변환 함 => 자동
-// 명시적 형 변환 : 개발자가 직접 함수 등을 이용해 형 변환 함 => 수동
+/*
+  * 형 변환 (Type Casting) : 어떤 값의 타입을 다른 타입으로 변경함
+  - 묵시적 형 변환 (암묵적 형 변환) => 자동 
+  - 명시적 형 변환 => 수동 
+*/
 
 // 1. 묵시적 형 변환
 // -> 자바스크립트 엔진이 알아서 형 변환 하는 것
@@ -9,23 +11,24 @@
 let num = 10;
 let str = "20";
 
-// 자동으로 numberType 에서 String Type으로 변환
+// 자동으로 Number Type 에서 String Type으로 변환
 const result = num + str;
 console.log(result);
 
 // 2. 명시적 형 변환
-// -> 프로그래며 내장함수 등을 이용해서 직접 형 변환을 명시
-// -> 문자열 -> 숫자
+// -> 프로그래머가 내장함수 등을 이용해서 직접 형 변환을 명시 (내장함수 : 자바스크립트가 기본적으로 제공하는 함수)
+// -> 문자열 --> 숫자
 let str1 = "10";
 let strToNum = Number(str1);
 console.log(10 + strToNum);
 
+// - 내장 함수 parseInt() : 문자열 안 숫자 외 다른 문자열 값이 포함되어있는 경우
+// (단, 숫자가 먼저 나와있는 경우만 형변환 가능)
 let str2 = "10개";
 let strToNum2 = parseInt(str2);
 console.log(strToNum2);
-// 숫자가 먼저 나와있는 경우만 형변환 가능
 
-// -> 숫자 -> 문자열
+// -> 숫자 --> 문자열
 let num1 = 20;
 let numToStr1 = String(num1);
 
