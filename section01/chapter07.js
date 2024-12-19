3ccfb91e441226c7f7ac2bdc25e02060862bb53b
sksla <153481660+sksla@users.noreply.github.com>
2024-12-19 22:12:29 +0900

    section01_ch07:연산자1
    대입 연산자
    산술 연산자
    복합 대입 연산자
    증감 연산자
    논리 연산자
    비교 연산자

--- section01/chapter07.js
+++ section01/chapter07.js
@@ -1,4 +1,4 @@
-// 연산자(Operator) : 프로그래밍에서의 다양한 연산을 위한 기호, 키워드
+// * 연산자(Operator) : 프로그래밍에서의 다양한 연산을 위한 기호, 키워드
 
 // 1. 대입 연산자(=) : 저장할 변수 = 저장할 값
 let var1 = 1;
@@ -10,23 +10,24 @@ let num3 = 3 * 2;
 let num4 = 3 / 2;
 let num5 = 3 % 2;
 
-// 연산 우선순위 있음
+// 연산 우선 순위
+// *, /, % >> +, -
 let num6 = (1 + 2) * 10;
 
-// 3. 복합 대입 연산자
+// 3. 복합 대입 연산자 (산술 + 대입)
 let num7 = 10;
-//num7 = num7 + 20;
-num7 += 20;
-num7 -= 20;
-num7 *= 20;
-num7 /= 20;
-num7 %= 20;
+num7 += 20; // num7 = num7 + 20
+num7 -= 20; // num7 = num7 - 20;
+num7 *= 20; // num7 = num7 * 20;
+num7 /= 20; // num7 = num7 / 20;
+num7 %= 20; // num7 = num7 % 20;
+
 console.log(num7);
 
-// 4. 증감 연산자(++, --)
+// 4. 증감 연산자(++, --) : 1씩 증감
 let num8 = 10;
 ++num8; // 전위 연산 : 현재 라인부터 바로 적용
-++num8; // 후위 연산 : 다음 라인부터 적용
+num8++; // 후위 연산 : 다음 라인부터 적용
 
 // 5. 논리 연산자(||, &&, !)
 let or = true || false;
@@ -38,18 +39,20 @@ let not = !true;
 console.log(or, and, not);
 
 // 6. 비교 연산자(===, !==, <, >, <=, >=)
-let comp1 = 1 === 2;
-let comp2 = 1 !== 2;
+// -> 동등 비교교
+let comp1 = 1 === 2; // false
+let comp2 = 1 !== 2; // true
 
 // === : 값의 자료형까지 같은지까지 비교(권장)
 // == : 값 자체만 비교
-console.log(1 === "1");
-console.log(1 == "1");
+console.log(1 === "1"); // false
+console.log(1 == "1"); // true
 
-let comp3 = 2 > 1;
-let comp4 = 2 < 1;
+// -> 대소 비교
+let comp3 = 2 > 1; // true
+let comp4 = 2 < 1; // false
 
-let comp5 = 2 >= 2;
-let comp6 = 2 <= 2;
+let comp5 = 2 >= 2; // true
+let comp6 = 2 <= 2; // true
 
 console.log(comp5, comp6);
