8040de143f634b995c5bd295d2f19cfc806d7934
sksla <153481660+sksla@users.noreply.github.com>
2024-12-28 14:01:49 +0900

    section01_ch15:객체1
    객체 생성
    객체 프로퍼티
    객체 프로퍼티를 다루는 방법

--- section01/chapter15.js
+++ section01/chapter15.js
@@ -1,7 +1,9 @@
 // 객체 1
-// 객체(Object) : 원시타입이 아닌 객체 타입의 자료형
-//               여러가지의 값을 동시에 저장 가능한 자료형
-// 현실 세계에 존재하는 어떤 사물이나 개념을 표현하기 용이함
+/*
+  객체(Object) : 원시타입이 아닌 객체 타입의 자료형
+                여러가지의 값을 동시에 저장 가능한 자료형
+  현실 세계에 존재하는 어떤 사물이나 개념을 표현하기에 용이함
+*/
 
 // 1. 객체 생성
 // 방법 1 : 객체 생성자
@@ -13,7 +15,7 @@ let obj2 = {};
 // 2. 객체 프로퍼티 (객체 속성)
 // key:value 형태
 // 프로퍼티의 개수 제한 없음, value 값의 타입제한 없음
-// key는 숫자 또는 문자로만 사용가능(단, 예외적으로 띄어쓰기가 있을 시 ""로 감싸는 거 허용)
+// key는 숫자 또는 문자로만 사용가능(단, 예외적으로 띄어쓰기가 있을 시 ""로 감싸야 함)
 let person = {
   // key : value
   name: "이정환",
@@ -29,24 +31,28 @@ let person = {
 // 3.1 특정 프로퍼티에 접근(점 표기법, 괄호표기법)
 // + 존재하지 않는 프로퍼티에 접근시 undefined 반환
 
-let name = person.name; // 점 표기법
-console.log(name);
+// > 점 표기법
+let name = person.name;
+console.log(name); // 줄 그어진 건 타입스크립트와 관련된 경고 사항(vscode의 기본기능, 지금은 무시해도 됨)
 
-let age = person["age"]; // 괄호표기법 : 접근하고자하는 key를 명시, ""로 꼭 감싸야함(문자열로!!)
+// > 괄호표기법 : 접근하고자하는 key를 명시, ""로 꼭 감싸야함(문자열로!!)
+//               따옴표로 감싸지 않으면 변수로 인식함
+let age = person["age"];
 console.log(age);
 
-// 동적으로 프로퍼티에 접근해야한다면 괄호표기법을 추천
+// + 동적으로 프로퍼티에 접근해야한다면 --> 괄호표기법을 추천
+//   그렇지 않다면 --> 간결한 점 표기법 추천
 let property = "hobby";
 let hobby = person[property];
 console.log(hobby);
 
 // 3.2 새로운 프로퍼티를 추가하는 방법(점 표기법, 괄호표기법)
-// 그냥 없던 프로퍼티에 값 추가하면 됨
+//     ==> 그냥 없던 프로퍼티에 값 추가하면 됨
 person.job = "fe developer";
 person["favoriteFood"] = "떡볶이";
 
 // 3.3 프로퍼티를 수정하는 방법
-// 있는 프로퍼티에 값 수정하면 됨
+//     ==> 있는 프로퍼티에 값 수정하면 됨
 person.job = "educator";
 person["favoriteFood"] = "초콜릿";
 
@@ -56,7 +62,8 @@ delete person["favoriteFood"];
 
 console.log(person);
 
-// 3.5 프로퍼티의 존재 유무를 확인하는 방법 (in 연산자 : 존재하면 true | 존재 X false 반환)
+// 3.5 프로퍼티의 존재 유무를 확인하는 방법
+//  => (in 연산자 : 존재하면 true | 존재 X false 반환)
 // "key" in 객체명
 let result1 = "name" in person;
 let result2 = "cat" in person;
